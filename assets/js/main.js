3810418156a7cd8680237c8df7655c5f22be8b71
Nicário <nicario.oliveira@gmail.com>
2022-06-27 16:29:20 -0300

    Adicionando ações

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-let a = document.querySelector(".projeto");
+const projetos = document.querySelector(".a__nav2");
+const quemSou = document.querySelector(".a__nav");
+const contato = document.querySelector(".a__nav3");
+
+projetos.addEventListener("click", function(a){
+  a.preventDefault();
+  window.scrollTo(0, 1000);
+});
+
+quemSou.addEventListener("click", function(b){
+  b.preventDefault();
+  window.scrollTo(0, 0);
+});
+
+contato.addEventListener("click", function(c){
+  c.preventDefault();
+  window.scrollTo(0, 1850);
+});
+
+
